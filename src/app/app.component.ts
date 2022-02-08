f954b4fec876224cf6bd35cc525692918a538d47
Manuel <manuel.brillantes@tawk.to>
2022-02-09 02:11:28 +0800

    Change to emit to callback

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 
-import { TawkMessengerAngular, TawkMessengerAngularModule } from 'tawk-messenger-angular';
+import { TawkMessengerAngular } from 'tawk-messenger-angular';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass'],
-  providers: [TawkMessengerAngular]
 })
 export class AppComponent implements OnInit {
-	title = 'tawk-messenger-angular';
+	title = 'tawk-messenger-angular-workspace';
 
 	gw = '';
 
@@ -22,9 +21,5 @@ export class AppComponent implements OnInit {
 			propertyId: '5f30f1a6d1b59c4448ec4d90',
 			widgetId: 'default'
 		});
-	}
-
-	onLoad() {
-		console.log('hello');
 	}
 }
